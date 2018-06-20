37ed5b982db800660ae65fcadb6b80f69e790f7f
Marcin <hu_man@o2.pl>
2018-06-20 13:34:36 +0200

    Add firebas 4.5.2 to Admob to resolve class conflict(admob greibase 5.0)

--- Assets/Scripts/Firebase/InitialiazeFirebase.cs
+++ Assets/Scripts/Firebase/InitialiazeFirebase.cs
@@ -10,18 +10,19 @@ public class InitialiazeFirebase : MonoBehaviour
 
     void Start()
     {
-        //Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
-        //    var dependencyStatus = task.Result;
-        //    if (dependencyStatus == Firebase.DependencyStatus.Available)
-        //    {
-        //        onFirebaseInitialiazed.Invoke();
-        //    }
-        //    else
-        //    {
-        //        UnityEngine.Debug.LogError(System.String.Format(
-        //          "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
-        //        // Firebase Unity SDK is not safe to use here.
-        //    }
-        //});
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                onFirebaseInitialiazed.Invoke();
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
     }
 }
--- Assets/Scripts/Firebase/Messages.cs
+++ Assets/Scripts/Firebase/Messages.cs
@@ -3,25 +3,25 @@
 public class Messages : MonoBehaviour
 {
 
-    //public void Init()
-    //{
-    //    Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
-    //    Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
-    //}
+    public void Init()
+    {
+        Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
+        Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
+    }
 
-    //public void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
-    //{
-    //    Debug.Log("Received Registration Token: " + token.Token);
-    //}
+    public void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
+    {
+        Debug.Log("Received Registration Token: " + token.Token);
+    }
 
-    //public void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
-    //{
-    //    Debug.Log("Received a new message from: " + e.Message.From);
-    //}
+    public void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
+    {
+        Debug.Log("Received a new message from: " + e.Message.From);
+    }
 
-    //private void OnDestroy()
-    //{
-    //    Firebase.Messaging.FirebaseMessaging.TokenReceived -= OnTokenReceived;
-    //    Firebase.Messaging.FirebaseMessaging.MessageReceived -= OnMessageReceived;
-    //}
+    private void OnDestroy()
+    {
+        Firebase.Messaging.FirebaseMessaging.TokenReceived -= OnTokenReceived;
+        Firebase.Messaging.FirebaseMessaging.MessageReceived -= OnMessageReceived;
+    }
 }
